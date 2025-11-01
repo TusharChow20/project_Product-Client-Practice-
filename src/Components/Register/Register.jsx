6e06449a5a93eead83f6092181760bf3278a86e0
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-11-01 16:47:26 +0600

    user login connect info with mongo

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -29,10 +29,26 @@ const Register = () => {
 
   const handleGoogleSignIn = () => {
     signInWithGoogle().then((result) => {
-      console.log(result).catch((error) => {
+      console.log(result.user)
+      const newUser={
+        name: result.user.displayName,
+        email : result.user.email,
+        image: result.user.photoURL
+
+      }
+      fetch('http://localhost:3000/users',{
+        method: "POST",
+        headers:{
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(newUser)
+
+      }).then(res=>res.json()).then(data=>console.log('data after user save',data)
+      )
+    }).catch((error) => {
         console.log(error);
       });
-    });
+    
   };
 
   return (
