d026630687472dd16ddd332faaf0a17803468b33
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-11-04 16:33:41 +0600

    custom token use

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -43,6 +43,21 @@ const AuthProvider = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+
+      if (currentUser) {
+
+        const loggedUser = { email: currentUser.email };
+        fetch("http://localhost:3000/getToken", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(loggedUser),
+        })
+          .then((res) => res.json())
+          .then((data) => console.log(data));
+      }
+
       setLoading(false);
     });
     return () => {
