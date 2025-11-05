2cd40195065df70e6abd669f77f9966223d7e7df
Tushar Chowdhury <tusharchowdhury20211@gmail.com>
2025-11-05 13:44:51 +0600

    axios intercceptor

--- src/CustomHook/useAxiosSecure.jsx
+++ src/CustomHook/useAxiosSecure.jsx
@@ -1,16 +1,35 @@
 import axios from "axios";
 import useAuth from "./useAuth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const instance = axios.create({
   baseURL: "http://localhost:3000",
 });
 const useAxiosSecure = () => {
-  const { user } = useAuth();
-  instance.interceptors.request.use((config) => {
-    console.log(config);
-    config.headers.authorization = `Bearer ${user.accessToken}`;
-    return config;
-  });
-  return instance;
+  const { user,logOut } = useAuth();
+  const navigate = useNavigate()
+  useEffect(() => {
+    const reqInterceptor = instance.interceptors.request.use((config) => {
+      console.log(config);
+      config.headers.authorization = `Bearer ${user.accessToken}`;
+      return config;
+    });
+
+    instance.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      (err) => {
+        if(err.status === 401 || err.status===403){
+          logOut().then(()=>{navigate('/login')})
+        }
+      },[user,logOut, navigate]
+    );
+    return () => {
+      instance.interceptors.request.eject(reqInterceptor);
+      instance.interceptors.response.eject(reqInterceptor);
+    };
+  }, [user]);
 };
 export default useAxiosSecure;
